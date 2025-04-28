d8b1980bdb6237d2cbca716d7302037acf6bb941
Arshdeep <harkiratkaur8c6@gmail.com>
2025-04-28 14:29:21 +0530

    Design product description page

--- public/app.js
+++ public/app.js
@@ -34,7 +34,16 @@ let images = document.querySelectorAll(".card-img-top");
 // Applying event listener to each image
 images.forEach(function(image){
    image.addEventListener("click", function(){
-     let form = this.closest("form");
-     form.submit();
+    let card = this.closest(".card"); 
+
+    let image =   card.querySelector("img"); // card.children[0] ;
+    let p1 = card.children[1].children[0];
+    let p2 = card.children[1].children[1];
+
+    let src = image.src;
+    let name = p1.innerText;
+    let price = p2.firstChild.textContent;
+
+    
    })
 });
