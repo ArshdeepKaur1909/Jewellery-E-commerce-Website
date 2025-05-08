051cc725159ffdfadd72e3173d0d6058cae3c333
Arshdeep <harkiratkaur8c6@gmail.com>
2025-05-08 22:45:09 +0530

    Change Root Directory to "/"

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/sophiaJewels", (req, res) => {
+app.get("/", (req, res) => {
   res.render("frontpage.ejs");
 });
 app.get("/necklace", (req, res) => {
